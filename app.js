0e357cd41f69e7be69a2ad372f66a34dc2ec86a3
ilkayus <ilkay.devecioglu@gmail.com>
2022-08-21 13:29:58 +0300

    removed some files

--- app.js
+++ app.js
@@ -3,16 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var cocktailsRouter = require("./routes/cocktailsRoutes");
 
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -33,8 +26,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/api/v1", cocktailsRouter);
 
 // catch 404 and forward to error handler
