673778e170c2319a81ec6c6d7af5020f96ad259b
ilkayus <ilkay.devecioglu@gmail.com>
2022-09-22 00:09:40 +0300

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,20 +39,15 @@ const createSendToken = (user, statusCode, res) => {
   res.status(statusCode).json({
     status: "success",
     token,
-    data: {
-      user,
-    },
+    user,
   });
 };
 
 exports.signIn = catchAsync(async (req, res, next) => {
   const { password, email } = req.body;
-  // 1- check if email and password exist
-  console.log(req.body);
   if (!email || !password) {
     return next(new AppError("Please provide email and password!", 400));
   }
-  // 2- check if user exists && password is correct
   const user = await User.findOne({ email }).select("+password");
   if (!user) {
     return next(new AppError("Incorrect email or password", 401));
@@ -61,7 +56,6 @@ exports.signIn = catchAsync(async (req, res, next) => {
   if (!correct) {
     return next(new AppError("Incorrect email or password", 401));
   }
-  // 3- if everything ok , send token to client
   const token = signToken(user._id);
   // response
   res.status(200).json({
